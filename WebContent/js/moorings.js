443155752ad518e2840c9fc55f03daf553c44dc2
PoloRiva <riva.polo@gmail.com>
2019-10-11 12:20:57 +0200

    created the Go to top button

--- WebContent/js/moorings.js
+++ WebContent/js/moorings.js
@@ -6,8 +6,9 @@ $(function() {
 //	var c = document.getElementsByClassName("rowOnMoorings");
 	console.log('Jquery working!');
 	
+	//Reads if each row has a child with class "text-danger" related to unavailable
+	//moorings, if true, it hides the cart
 	let c = document.getElementsByClassName("rowOnMoorings");
-	
 	for (var i=0; i<c.length; i++) {
 		//let a = $(c[i]).child();
 		//console.log(c[i].getElementsByClassName("text-danger"));
@@ -16,6 +17,21 @@ $(function() {
 			$(a).hide();
 		}
 	}
+	
+	//If the element with class="linkButton" is clicked, a scroll is executed
+	//reaching the top of the page
+	$(".linkBottom").click(function() {
+		console.log("click on go to top");
+		$(window).scrollTop(0);
+	});
+	
+	//Changes the bottom message of pages "Go to top" colors when the mouse is over
+	$(".linkBottom").mouseenter(function() {
+		$(".linkBottom").attr("style","color: red;");
+	}).mouseleave(function() {
+		$(".linkBottom").attr("style","color: blue;");
+	});
+	
 });
 
 
