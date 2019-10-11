e4d60ccfeeb1b0d5248944b039b3894ae5d16bf5
PoloRiva <riva.polo@gmail.com>
2019-10-11 10:18:54 +0200

    morring cart hide if the morring is unavailable

--- WebContent/js/moorings.js
+++ WebContent/js/moorings.js
@@ -5,14 +5,17 @@
 $(function() {
 //	var c = document.getElementsByClassName("rowOnMoorings");
 	console.log('Jquery working!');
-	console.log('Jquery working!');
-	console.log('Jquery working!');
 	
+	let c = document.getElementsByClassName("rowOnMoorings");
 	
-	$("#Test").click(function() {
-		console.log('Test confirmed');
-	});
-	
+	for (var i=0; i<c.length; i++) {
+		//let a = $(c[i]).child();
+		console.log(c[i].getElementsByClassName("text-danger"));
+		if(c[i].getElementsByClassName("text-danger").length != 0) {
+			let a = c[i].getElementsByClassName("cartMooring");
+			$(a).hide();		
+		}
+	}
 });
 
 
