a6371154713090ec2cb2f8029bd73329f3645cb3
PoloRiva <riva.polo@gmail.com>
2019-10-11 09:27:10 +0200

    mooring work

--- WebContent/js/moorings.js
+++ WebContent/js/moorings.js
@@ -5,9 +5,11 @@
 $(function() {
 //	var c = document.getElementsByClassName("rowOnMoorings");
 	console.log('Jquery working!');
+	console.log('Jquery working!');
+	console.log('Jquery working!');
 	
 	
-	$("#Test").hover(function() {
+	$("#Test").click(function() {
 		console.log('Test confirmed');
 	});
 	
